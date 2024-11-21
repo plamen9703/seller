cc6afd98dcefe6aa4b6893c853088546d144c5dc
krumks <krumks@LAB28-11.school.npmg.org>
2024-11-21 16:14:02 +0200

    Second_commit

---
+++ sellers/src/sellers/Main.java
@@ -0,0 +1,11 @@
+package sellers;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Sellers s = new Sellers();
+		System.out.println(s);
+	}
+
+}
